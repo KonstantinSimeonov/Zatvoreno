c8f5e33f92646bc44c695038641aa39897d3f2b7
PavelNPavlov <pavelnapavlov@gmail.com>
2015-12-06 14:12:35 +0200

    interface

--- SantaseGameEngine/ZatvorenoAI/TakeStrategy/Agents/ChoseTake/Contracts/IChoseAction.cs
+++ SantaseGameEngine/ZatvorenoAI/TakeStrategy/Agents/ChoseTake/Contracts/IChoseAction.cs
@@ -1,6 +1,11 @@
 ﻿namespace ZatvorenoAI.TakeStrategy.Agents.ChoseTake.Contracts
 {
+    using Santase.Logic.Cards;
+    using Santase.Logic.Players;
+    using System.Collections.Generic;
+
     public interface IChoseAction
     {
+        Card CardToPlay(PlayerTurnContext context, ICollection<Card> hand);
     }
 }
