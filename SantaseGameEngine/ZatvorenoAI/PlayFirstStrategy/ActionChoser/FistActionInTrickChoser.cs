8f6f25f2752ca4d76cd60d16f00afa69c6aa73be
PavelNPavlov <pavelnapavlov@gmail.com>
2015-12-07 05:40:40 +0200

    current setting 48 %

--- SantaseGameEngine/ZatvorenoAI/PlayFirstStrategy/ActionChoser/FistActionInTrickChoser.cs
+++ SantaseGameEngine/ZatvorenoAI/PlayFirstStrategy/ActionChoser/FistActionInTrickChoser.cs
@@ -25,7 +25,7 @@
 
         public KeyValuePair<bool, Card> CardToPlayAndCloseLogic(PlayerTurnContext context, ICollection<Card> hand)
         {
-            var evaluatedOption = optionEval.EvaluateSituation(context, hand);
+            var evaluatedOption = this.optionEval.EvaluateSituation(context, hand);
             if (evaluatedOption.IsEndGame)
             {
                 var optimalOptions = evaluatedOption.CardStats.Where(x => x.CanBeTakenCount == 0)
@@ -49,11 +49,21 @@
                     return new KeyValuePair<bool, Card>(true, optimalOptions.First().Card);
                 }
 
-                if (evaluatedOption.PlayerUnder33 && evaluatedOption.OpponetAbove50)
+
+                if (evaluatedOption.OpponetAbove50)
                 {
                     var optimalOption = evaluatedOption.CardStats
                                         .OrderByDescending(x => x.CardWorth)
                                         .First().Card;
+
+                    return new KeyValuePair<bool, Card>(false, optimalOption);
+                }
+
+                if(evaluatedOption.PlayerUnder33 && context.CardsLeftInDeck>6)
+                {
+                    var optimalOption = evaluatedOption.CardStats
+                                       .OrderByDescending(x => x.CardWorth)
+                                       .First().Card;
 
                     return new KeyValuePair<bool, Card>(false, optimalOption);
                 }
